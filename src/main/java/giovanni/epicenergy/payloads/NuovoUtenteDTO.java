32ae9203bf1988894615ee0cbe791171351b31a3
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-07-08 21:49:32 +0200

    fix NuovoUtenteDTO

--- src/main/java/giovanni/epicenergy/payloads/NuovoUtenteDTO.java
+++ src/main/java/giovanni/epicenergy/payloads/NuovoUtenteDTO.java
@@ -1,21 +1,26 @@
 package giovanni.epicenergy.payloads;
 
-import giovanni.epicenergy.enums.TipoUtenteENUM;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record NuovoUtenteDTO(
-        TipoUtenteENUM ruolo,
+        @NotEmpty(message = "Ii ruolo è obbligatorio!")
+        String tipoUtenteENUM,
         @NotEmpty(message = "Lo username è obbligatorio!")
-        @Size(min = 3, max = 25, message = "La password deve essere compresa tra 3 e 25 caratteri!")
+        @Size(min = 3, max = 25, message = "Lo username deve essere compreso tra 3 e 25 caratteri!")
         String userName,
-        @NotEmpty(message = "Lo username è obbligatorio!")
+        @NotEmpty(message = "L'email è obbligatoria!")
+        @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Email non valida")
         String email,
-        @NotEmpty(message = "Lo username è obbligatorio!")
+        @NotEmpty(message = "La password è obbligatoria!")
+        @Size(min = 3, max = 25, message = "La password deve essere compresa tra 3 e 25 caratteri!")
         String password,
-        @NotEmpty(message = "Lo username è obbligatorio!")
+        @NotEmpty(message = "Il nome è obbligatorio!")
+        @Size(min = 3, max = 25, message = "Il nome deve essere compreso tra 3 e 25 caratteri!")
         String nome,
-        @NotEmpty(message = "Lo username è obbligatorio!")
+        @NotEmpty(message = "Il cognome è obbligatorio!")
+        @Size(min = 3, max = 25, message = "Il cognome deve essere compreso tra 3 e 25 caratteri!")
         String cognome
 ) {
 }
