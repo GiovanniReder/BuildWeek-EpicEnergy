0bc766219b0b9cef1a80e93a7ff74e7d75685f6d
Giovanni <gio.reder@gmail.com>
2024-07-09 15:41:57 +0200

    create exception authorization

--- src/main/java/giovanni/epicenergy/entities/Utente.java
+++ src/main/java/giovanni/epicenergy/entities/Utente.java
@@ -80,4 +80,6 @@ public class Utente implements UserDetails  {
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
     }
+
+
 }
---
+++ src/main/java/giovanni/epicenergy/exceptions/AuthorizationDeniedException.java
@@ -0,0 +1,12 @@
+package giovanni.epicenergy.exceptions;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class AuthorizationDeniedException extends RuntimeException{
+    public AuthorizationDeniedException(String message) {
+        super(message);
+    }
+
+}
