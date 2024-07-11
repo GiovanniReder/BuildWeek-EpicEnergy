1833e3394e881cdc8da99266ee376675f3002742
Giovanni <gio.reder@gmail.com>
2024-07-11 22:10:33 +0200

    add filter by import

---
+++ src/main/java/giovanni/epicenergy/controllers/FatturaController.java
@@ -0,0 +1,49 @@
+package giovanni.epicenergy.controllers;
+
+import giovanni.epicenergy.entities.Fattura;
+import giovanni.epicenergy.payloads.fatture.NuovaStatoFatturaDTO;
+import giovanni.epicenergy.payloads.filtri.DataInserimentoDTO;
+import giovanni.epicenergy.payloads.filtri.FatturatoDTO;
+import giovanni.epicenergy.services.FatturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/fatture")
+public class FatturaController {
+    @Autowired
+    private FatturaService fatturaService;
+
+    @GetMapping("/stato")
+
+    public Page<Fattura> filterByStatoFattura(@RequestBody NuovaStatoFatturaDTO body,
+                                              @RequestParam(defaultValue = "0") int page,
+                                              @RequestParam(defaultValue = "10") int size,
+                                              @RequestParam(defaultValue = "data") String  sortBy){
+
+        return fatturaService.fatturaPerStato(body,page,size,sortBy);
+    }
+    @GetMapping("/data")
+    public Page<Fattura> filterByData(@RequestBody DataInserimentoDTO body,
+                                      @RequestParam(defaultValue = "0") int page,
+                                      @RequestParam(defaultValue = "10") int size,
+                                      @RequestParam(defaultValue = "data") String  sortBy
+                                      ){
+        return fatturaService.fatturaPerData(body, page, size, sortBy);
+    }
+
+    @GetMapping("/importo")
+    public Page<Fattura> filterByImporto(
+            @RequestBody FatturatoDTO body,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "data") String  sortBy
+            ){
+        return fatturaService.fatturaPerImporto(body,page, size, sortBy);
+    }
+
+
+
+
+}
--- src/main/java/giovanni/epicenergy/repositories/fatture/FatturaRepository.java
+++ src/main/java/giovanni/epicenergy/repositories/fatture/FatturaRepository.java
@@ -1,13 +1,31 @@
 package giovanni.epicenergy.repositories.fatture;
 
+import giovanni.epicenergy.entities.Cliente;
 import giovanni.epicenergy.entities.Fattura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface FatturaRepository extends JpaRepository<Fattura , UUID> {
     Optional<Fattura>findByNumero(long numero);
+
+    @Query("SELECT f FROM Fattura f WHERE f.stato.stato ILIKE :statoFattura ")
+    Page<Fattura> filterByStatoFattura(String statoFattura,
+                                    Pageable pageable);
+    @Query("SELECT f FROM Fattura f WHERE f.data BETWEEN :dataOne AND :dataTwo   ")
+    Page<Fattura> filterByData(LocalDate dataOne, LocalDate dataTwo,
+                               Pageable pageable);
+
+    @Query("SELECT f FROM Fattura f WHERE f.importo BETWEEN :rangeOne AND :rangeTwo")
+    Page<Fattura> filterByImporto( long rangeOne,
+                                     long rangeTwo,
+                                     Pageable pageable);
+
 
 }
