f5961c682ddae4c77e171a5f3c2a96bcba94ff7a
FocusFine89 <niivanov02@gmail.com>
2024-07-11 10:06:45 +0200

    Validazione DTO

--- src/main/java/giovanni/epicenergy/payloads/clienti/NuovoClienteDTO.java
+++ src/main/java/giovanni/epicenergy/payloads/clienti/NuovoClienteDTO.java
@@ -1,24 +1,41 @@
 package giovanni.epicenergy.payloads.clienti;
 
 import giovanni.epicenergy.enums.RagioneSocialeENUM;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record NuovoClienteDTO(
+        @NotEmpty
         RagioneSocialeENUM ragioneSociale,
+        @NotNull
+        @Size(min = 11, max = 11, message = "La partita iva deve essere di 11 cifre")
         long partitaIva,
+        @NotEmpty
         String email,
         LocalDate dataInserimento,
         LocalDate dataUltimoContatto,
+        @NotNull
         long fatturatoAnnuale,
+        @NotEmpty
         String pec,
+        @NotEmpty
         String emailContatto,
+        @NotEmpty
         String nomeContatto,
+        @NotNull
+        @Size(min = 10, max = 10, message = "Il numero di telefono deve contenere 10 cifre")
         long telefonoContatto,
         String logoAziendale,
+        @NotEmpty
         String via,
+        @NotEmpty
         String civico,
+        @NotEmpty
         String cap,
+        @NotEmpty
         String comune
         ) {
 }
--- src/main/java/giovanni/epicenergy/payloads/fatture/NuovaFatturaDTO.java
+++ src/main/java/giovanni/epicenergy/payloads/fatture/NuovaFatturaDTO.java
@@ -1,7 +1,21 @@
 package giovanni.epicenergy.payloads.fatture;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record NuovaFatturaDTO(long numero, LocalDate data , long importo, UUID clienteId) {
+public record NuovaFatturaDTO(
+        @NotNull
+        @Size(min = 10, message = "Il numero deve contenere un minimo di 10 Numeri")
+        long numero,
+        LocalDate data ,
+        @NotEmpty
+        @NotNull
+        long importo,
+        @NotNull
+        UUID clienteId
+) {
 }
