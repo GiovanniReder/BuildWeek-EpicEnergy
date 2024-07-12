5b918a3b7064a8ca99eda4727e10e4ea1c4138e4
CristianMartu <martuccicristian18@gmail.com>
2024-07-12 14:42:47 +0200

    commit

--- src/main/java/giovanni/epicenergy/services/UtenteService.java
+++ src/main/java/giovanni/epicenergy/services/UtenteService.java
@@ -41,10 +41,10 @@ public class UtenteService {
     private MailgunSender mailgunSender;
 
     public Utente save(NuovoUtenteDTO body) {
-       // this.utenteRepository.findByEmail(body.email()).ifPresent(
-            //    user -> {
-              //      throw new BadRequestException("email già in uso");
-//});
+        this.utenteRepository.findByEmail(body.email()).ifPresent(
+                user -> {
+                    throw new BadRequestException("email già in uso");
+        });
         Utente newUser = new Utente(body.cognome(), body.nome(), bcrypt.encode(body.password()), body.email(),
                 body.userName());
         newUser.setRuoli(new ArrayList<>(Arrays.asList(ruoloUtenteService.findByRuolo("USER"))));
